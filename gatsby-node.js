80f16228e5751bfc5b56cde18e5ed4a22af40182
tyebanks <ebankst07@gmail.com>
2020-08-18 06:22:04 +0100

    files removed so new gatsby starter can be cloned

--- gatsby-node.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// Gatsby gives plugins and site builders many APIs for controlling your site’s data in the GraphQL data layer.
-// This file can be deleted if not being used.
