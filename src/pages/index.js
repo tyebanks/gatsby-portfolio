90daf761afc241b11570c620d0bf8ae2b630c694
tyebanks <ebankst07@gmail.com>
2020-08-18 08:18:27 +0100

    changes to technical skills

--- src/pages/index.js
+++ src/pages/index.js
@@ -77,6 +77,67 @@ const IndexPage = () => (
       </div>
     </header>
 
+    <section className="page-section" id="services">
+      <div className="container">
+        <h2 className="text-center mt-0">Technical Skills</h2>
+        <hr className="divider my-4" />
+        <div className="row">
+          <div className="col-lg-3 col-md-6 text-center">
+            <div className="mt-5">
+              <i className="fas fa-4x fa-gem text-primary mb-4"></i>
+              <h3 className="h4 mb-2">Sturdy Themes</h3>
+              <p className="text-muted mb-0">
+                Our themes are updated regularly to keep them bug free!
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 text-center">
+            <div className="mt-5">
+              <i className="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+              <p className="text-muted mb-0">HTML5 </p>
+              <p className="text-muted mb-0">CSS3</p>
+              <p className="text-muted mb-0">JavaScript</p>
+              <p className="text-muted mb-0">PHP</p>
+              <p className="text-muted mb-0">Joomla</p>
+                      <p className="text-muted mb-0">WordPress</p>
+                      <p className="text-muted mb-0">Atom</p>
+                      <p className="text-muted mb-0">VS Code </p>
+                      <p className="text-muted mb-0">Java/Eclipse</p>
+                      <p className="text-muted mb-0">GitHub</p>
+            
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 text-center">
+            <div className="mt-5">
+              <i className="fas fa-4x fa-globe text-primary mb-4"></i>
+              <p className="text-muted mb-0">Adobe Illustrator </p>
+              <p className="text-muted mb-0">Adobe Photoshop</p>
+              <p className="text-muted mb-0">Figma</p>
+              <p className="text-muted mb-0">inVision</p>
+              <p className="text-muted mb-0">Discord</p>
+                      <p className="text-muted mb-0">Slack</p>
+                      <p className="text-muted mb-0">Skype</p>
+                      <p className="text-muted mb-0">GatsbyJS </p>
+                      <p className="text-muted mb-0">ReactJS</p>
+                      <p className="text-muted mb-0">OneDrive</p>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 text-center">
+            <div className="mt-5">
+              <i className="fas fa-4x fa-heart text-primary mb-4"></i>
+              <h3 className="h4 mb-2">Made with Love</h3>
+              <p className="text-muted mb-0">
+                Is it really open source if it's not made with love?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
     <section className="page-section bg-dark text-white">
       <div className="container text-center">
         <h2 className="mb-4">My Projects!</h2>
@@ -101,15 +162,15 @@ const IndexPage = () => (
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-8 text-center">
-            <h2 className="text-white mt-0">Education</h2>
+            <h2 className="text-black mt-0">Education</h2>
             <hr className="divider light my-4" />
-            <p className="text-white-50 mb-4">
+            <p className="text-black-50 mb-4">
             BSc Business Computing (Web Design)
             University of Northampton
             Northampton, U.K.
             NN1 5PH
             </p>
-            <p className="text-white-50 mb-4">AAS degree in Computer Science
+            <p className="text-black-50 mb-4">AAS degree in Computer Science
             University College Cayman Islands
             George Town, K.Y.
             KY1-1107 
@@ -127,51 +188,7 @@ const IndexPage = () => (
       </div>
     </section>
 
-    <section className="page-section" id="services">
-      <div className="container">
-        <h2 className="text-center mt-0">Technical Skills</h2>
-        <hr className="divider my-4" />
-        <div className="row">
-          <div className="col-lg-3 col-md-6 text-center">
-            <div className="mt-5">
-              <i className="fas fa-4x fa-gem text-primary mb-4"></i>
-              <h3 className="h4 mb-2">Sturdy Themes</h3>
-              <p className="text-muted mb-0">
-                Our themes are updated regularly to keep them bug free!
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 text-center">
-            <div className="mt-5">
-              <i className="fas fa-4x fa-laptop-code text-primary mb-4"></i>
-              <h3 className="h4 mb-2">Up to Date</h3>
-              <p className="text-muted mb-0">
-                All dependencies are kept current to keep things fresh.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 text-center">
-            <div className="mt-5">
-              <i className="fas fa-4x fa-globe text-primary mb-4"></i>
-              <h3 className="h4 mb-2">Ready to Publish</h3>
-              <p className="text-muted mb-0">
-                You can use this design as is, or you can make changes!
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 text-center">
-            <div className="mt-5">
-              <i className="fas fa-4x fa-heart text-primary mb-4"></i>
-              <h3 className="h4 mb-2">Made with Love</h3>
-              <p className="text-muted mb-0">
-                Is it really open source if it's not made with love?
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
+   
    
 
   
