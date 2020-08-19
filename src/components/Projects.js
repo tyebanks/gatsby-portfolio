5ba3d03dc86bb687de9703120c94ec8c9b379ee3
tyebanks <ebankst07@gmail.com>
2020-08-19 01:13:35 +0100

    projects page fully responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Projects = (props) => {
     return (
         <div className="project-card py-5">
-            <p className="">{props.title}</p>
-            <p className="">{props.subtitle1}</p>
+            <p className="project-title">{props.title}</p>
+            <p className="project-subtitle">{props.subtitle1}</p>
             <img id="project-imgs" className="img-fluid" src={props.src} alt={props.description}/>
         </div>        
     )
